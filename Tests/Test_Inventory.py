f04950bae4b9671a36f596e64de6fd88f2870ea7
mmeerrkkaa <merkkaa81@gmail.com>
2022-09-24 12:03:50 +0300

    Update Test_Inventory.py

--- Tests/Test_Inventory.py
+++ Tests/Test_Inventory.py
@@ -1,47 +1,47 @@
 import pytest
 
-from player.Player import Player
+from player.Inventory import Inventory
 
 def tests_add_item():
-	player = Player("Test")
-	player.inventory.add_item("Камень")
-	assert player.inventory.get_item("Камень")['count'] == 1
+	inventory = Inventory()
+	inventory.add_item("Камень")
+	assert inventory.get_item("Камень")['count'] == 1
 
-	player.inventory.add_item("Камень", 2)
-	assert player.inventory.get_item("Камень")['count'] == 3
+	inventory.add_item("Камень", 2)
+	assert inventory.get_item("Камень")['count'] == 3
 
-	player.inventory.add_item("Камень", 2)
-	assert player.inventory.get_item("Камень")['count'] == 5
+	inventory.add_item("Камень", 2)
+	assert inventory.get_item("Камень")['count'] == 5
 
 
 def tests_remove_item():
-	player = Player("Test")
-	player.inventory.add_item("Камень", 5)
-	player.inventory.remove_item("Камень", 2)
-	assert player.inventory.get_item("Камень") == {"count": 3}
+	inventory = Inventory()
+	inventory.add_item("Камень", 5)
+	inventory.remove_item("Камень", 2)
+	assert inventory.get_item("Камень") == {"count": 3}
 
-	player.inventory.remove_item("Камень", 3)
+	inventory.remove_item("Камень", 3)
 
 	with pytest.raises(Exception):
-		player.inventory.get_item("Камень")
+		inventory.get_item("Камень")
 
 
 def tests_get_item():
-	player = Player("Test")
-	player.inventory.add_item("Камень", 5)
-	assert player.inventory.get_item("Камень") == {"count": 5}
+	inventory = Inventory()
+	inventory.add_item("Камень", 5)
+	assert inventory.get_item("Камень") == {"count": 5}
 
 
 def tests_get_items():
-	player = Player("Test")
-	player.inventory.add_item("Камень", 5)
-	assert player.inventory.get_items() == {"Камень": {"count": 5}}
+	inventory = Inventory()
+	inventory.add_item("Камень", 5)
+	assert inventory.get_items() == {"Камень": {"count": 5}}
 
-	player.inventory.add_item("Камень", 5)
-	assert player.inventory.get_items() == {"Камень": {"count": 10}}
+	inventory.add_item("Камень", 5)
+	assert inventory.get_items() == {"Камень": {"count": 10}}
 
-	player.inventory.add_item("Палка")
-	assert player.inventory.get_items() == {"Камень": {"count": 10}, "Палка": {"count": 1}}
+	inventory.add_item("Палка")
+	assert inventory.get_items() == {"Камень": {"count": 10}, "Палка": {"count": 1}}
 
 
 if __name__ == "__main__":
