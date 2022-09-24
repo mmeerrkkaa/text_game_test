93b2a5b5760482da9054a9847eb92e9852f2431c
mmeerrkkaa <merkkaa81@gmail.com>
2022-09-24 11:56:30 +0300

    Изменения структуры инвентаря

--- player/Inventory.py
+++ player/Inventory.py
@@ -1,24 +1,27 @@
 class Inventory:
 	def __init__(self):
-		self.items = {"Камень": {"count": 1, "rare": "Обычный"}}
+		self.items = {}
 
-	def add_item(self, item_name: str, count: int = 1, rare: str = "Обычный"):
+	def add_item(self, item_name: str, count: int = 1):
 		""" Добавляет предмет в инвентарь если его нету. Если есть, то добавляет к существующему """
-		if item.name in self.items:
+		if item_name in self.items:
 			self.items[item_name]["count"] += count
 		else:
-			self.items[item_name] = {"count": count, "rare": item.rare}
+			self.items[item_name] = {"count": count}
 
 	def remove_item(self, item_name: str, count: int = 1):
 		""" Убирает предмет из инвентаря если он есть. Если нет, то выдает ошибку """
-		if item.name in self.items:
-			self.items[item_name]["count"] -= count
+		if item_name in self.items:
+			if self.items[item_name]["count"] > count:
+				self.items[item_name]["count"] -= count
+			else:
+				del self.items[item_name]
 		else:
 			raise Exception("Предмета нет в инвентаре")
 
 	def get_item(self, item_name: str):
 		""" Возвращает предмет из инвентаря. Если его нет, то выдает ошибку """
-		if item.name in self.items:
+		if item_name in self.items:
 			return self.items[item_name]
 		else:
 			raise Exception("Предмета нет в инвентаре")
