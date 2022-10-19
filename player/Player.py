060238d351556e6209e94a15f2afbdf8a2d90a9e
anton-hk <gikalanton1@gmail.com>
2022-10-19 10:23:44 +0300

    удалил свою вчерашнюю функцию, сделал новую, тестами не покрывал, пока не знаю как это делается

--- player/Player.py
+++ player/Player.py
@@ -60,20 +60,24 @@ class Player:
 	def remove_money(self, money):
 		self.money -= money
 
-	def use_item_heal_potion(self):
-		"""Использует предмет из инвентаря - зелье восстановления здоровья"""
-		item = self.inventory.get_item('heal_potion')		# возможно стоит heal_potion передавать на вход метода
-		#Если метод get_item() выдаст ошибку не знаю что будет. Прервется ли выполнение метода и пойдет
-		# отловка ошибок которая скажет игроку о том что у него нет такого предмета?
-		self.health += self.max_health*0.2		# зелье лечения восстанавливает 20% общего запаса здоровья
-		if self.health > self.max_health:
-			self.health = self.max_health
-		self.inventory.remove_item(item)
+	def use_item(self, item: str):
+		""" Использует предмет из инвентаря"""
+		self.inventory.get_item(item)
+		if item == 'heal_potion':
+			self.health += self.max_health*0.2
+			if self.health > self.max_health:
+				self.health = self.max_health
+			self.inventory.remove_item(item, 1)
 
-	def test_felix(self):
-		self.health = self.health
+		elif item == 'grindstone':
+			self.damage += 10
+			self.inventory.remove_item(item, 1)
 
-
+		elif item == 'metal':
+			self.defense += 10
+			self.inventory.remove_item(item, 1)
+		else:
+			raise Exception("Неизвестный предмет")
 
 
 
